30530c1cbabb9212f2bd9990aa27b36709f82bd5
David Hein <david-hein196@web.de>
2019-11-10 22:25:29 +0100

    Add config for receiver and salutation

---
+++ src/config.php
@@ -0,0 +1,8 @@
+<?php
+
+$config['receiver_name'] = "Akademie für Muster";
+$config['receiver_street'] = "Musterstraße 1";
+$config['receiver_city'] = "12345 Musterstadt";
+$config['salutation'] = "Sehr geehrter Herr Mustermann";
+
+?>
