8e19ead433cf427da6878ad9f0e80415aba5179e
David Hein <david-hein196@web.de>
2019-11-13 13:41:53 +0100

    Add fields to form. Add labels.

--- src/apologyMinutes.php
+++ src/apologyMinutes.php
@@ -31,10 +31,37 @@
             id="city" name="city"
             placeholder="Stadt" required><br>
 
-        <input type="date" name="absenceDate" value="<?php echo date(d.m.Y); ?>">
-        <input type="time" name="time_from" placeholder="Abwesend von">
-        <input type="time" name="time_to" placeholder="Abwesend bis">
-    <button>Erstellen</button>
-  </form>
+        <label for="absenceDate">Abwesenheitsdatum:</label><br>
+        <input type="date"
+            id="absenceDate" name="absenceDate"
+            placeholder="z.B. 11.11.2019"
+            value="<?php echo date('d.m.Y'); ?>" required><br>
+
+        <label for="time_from">Abwesend von:</label><br>
+        <input type="time"
+            id="time_from" name="time_from"
+            placeholder="z.B. 8:00, 16:05"
+            pattern="(2[0-3])|([0-1]?[0-9]):[0-5][0-9]" required><br>
+
+        <label for="time_to">Abwesend bis:</label><br>
+        <input type="time"
+            id="time_to" name="time_to"
+            placeholder="z.B. 8:00, 16:05"
+            pattern="(2[0-3])|([0-1]?[0-9]):[0-5][0-9]" required><br>
+
+        <label for="explanation">Grund:</label><br>
+        <input type="text"
+            id="explanation" name="explanation"
+            placeholder="Ich habe die Zeit vergessen" required><br>
+
+        <label for="typeOfDelay">Typ der Verspätung:</label><br>
+        <select id="typeOfDelay" name="typeOfDelay">
+            <option value="Ich habe mich <strong>verspätet</strong>" selected>Ich habe mich <strong>verspätet</strong></option>
+            <option value="Ich bin <strong>früher gegangen</strong>">Ich bin <strong>früher gegangen</strong></option>
+            <option value="Ich bin <strong>später gekommen</strong>">Ich bin <strong>später gekommen</strong></option>
+        </select><br>
+
+        <button>Erstellen</button>
+    </form>
 </body>
 </html>
