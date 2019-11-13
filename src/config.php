ebb5cc3f31c910e524ae7ed632b0869e7ada5d6e
David Hein <david-hein196@web.de>
2019-11-13 09:06:08 +0100

    Split city and postal code

--- src/config.php
+++ src/config.php
@@ -2,7 +2,8 @@
 
 $config['receiver_name'] = "Akademie für Muster";
 $config['receiver_street'] = "Musterstraße 1";
-$config['receiver_city'] = "12345 Musterstadt";
+$config['receiver_postalCode'] = "12345";
+$config['receiver_city'] = "Musterstadt";
 $config['salutation'] = "Sehr geehrter Herr Mustermann";
 
 ?>
