fa700374290593001b62c16f040148e563875f22
jones1008 <jones@fluhrers.de>
2019-11-15 15:36:01 +0100

    put input fields in input-groups with caption

--- src/apologyMinutes.php
+++ src/apologyMinutes.php
@@ -16,42 +16,45 @@
 
             <?php include("personalInfos.php") ?>
 
-            <label for="absenceDate">Abwesenheitsdatum:</label><br>
-            <input type="date"
-                id="absenceDate" name="absenceDate"
-                placeholder="z.B. 11.11.2019"
-                value="<?php echo date('Y-m-d'); ?>" required><br>
+            <div class="input-group">
+                <h4 class="input-group-caption">Informationen zur Abwesenheit</h4>
 
-            <div class="row">
-                <div class="col-2 input-field">
-                    <label for="time_from">Abwesend von:</label>
-                    <input type="time"
-                        id="time_from" name="time_from"
-                        placeholder="z.B. 8:00, 16:05"
-                        pattern="(2[0-3])|([0-1]?[0-9]):[0-5][0-9]" required>
+                <label for="absenceDate">Abwesenheitsdatum:</label><br>
+                <input type="date"
+                       id="absenceDate" name="absenceDate"
+                       placeholder="z.B. 11.11.2019"
+                       value="<?php echo date('Y-m-d'); ?>" required><br>
+
+                <div class="row">
+                    <div class="col-2 input-field">
+                        <label for="time_from">Abwesend von:</label>
+                        <input type="time"
+                               id="time_from" name="time_from"
+                               placeholder="z.B. 8:00, 16:05"
+                               pattern="(2[0-3])|([0-1]?[0-9]):[0-5][0-9]" required>
+                    </div>
+
+                    <div class="col-2 input-field">
+                        <label for="time_to">Abwesend bis:</label>
+                        <input type="time"
+                               id="time_to" name="time_to"
+                               placeholder="z.B. 8:00, 16:05"
+                               pattern="(2[0-3])|([0-1]?[0-9]):[0-5][0-9]" required>
+                    </div>
                 </div>
 
-                <div class="col-2 input-field">
-                    <label for="time_to">Abwesend bis:</label>
-                    <input type="time"
-                        id="time_to" name="time_to"
-                        placeholder="z.B. 8:00, 16:05"
-                        pattern="(2[0-3])|([0-1]?[0-9]):[0-5][0-9]" required>
-                </div>
+                <label for="explanation">Grund:</label><br>
+                <input type="text"
+                       id="explanation" name="explanation"
+                       placeholder="Ich habe die Zeit vergessen" required><br>
+
+                <label for="typeOfDelay">Typ der Verspätung:</label><br>
+                <select id="typeOfDelay" name="typeOfDelay">
+                    <option value="Ich habe mich <strong>verspätet</strong>" selected>Ich habe mich <strong>verspätet</strong></option>
+                    <option value="Ich bin <strong>früher gegangen</strong>">Ich bin <strong>früher gegangen</strong></option>
+                    <option value="Ich bin <strong>später gekommen</strong>">Ich bin <strong>später gekommen</strong></option>
+                </select><br>
             </div>
-
-            <label for="explanation">Grund:</label><br>
-            <input type="text"
-                id="explanation" name="explanation"
-                placeholder="Ich habe die Zeit vergessen" required><br>
-
-            <label for="typeOfDelay">Typ der Verspätung:</label><br>
-            <select id="typeOfDelay" name="typeOfDelay">
-                <option value="Ich habe mich <strong>verspätet</strong>" selected>Ich habe mich <strong>verspätet</strong></option>
-                <option value="Ich bin <strong>früher gegangen</strong>">Ich bin <strong>früher gegangen</strong></option>
-                <option value="Ich bin <strong>später gekommen</strong>">Ich bin <strong>später gekommen</strong></option>
-            </select><br>
-
             <button class="button round">Erstellen</button>
         </form>
     </div>
--- src/personalInfos.php
+++ src/personalInfos.php
@@ -1,38 +1,40 @@
-<div class="row">
-    <div class="col-2 input-field">
-        <label for="firstname">Vorname:</label>
-        <input type="text"
-               id="firstname" name="firstname"
-               placeholder="Vorname" required autofocus>
-    </div>
-    <div class="col-2 input-field">
-        <label for="lastname">Nachname:</label>
-        <input type="text"
-               id="lastname" name="lastname"
-               placeholder="Nachname" required>
-    </div>
-</div>
-<hr>
-
-<div class="address">
-    <label for="street">Straße:</label><br>
-    <input type="text"
-           id="street" name="street"
-           placeholder="Straße und Hausnummer" required><br>
-
+<div class="input-group">
+    <h4 class="input-group-caption">Persönliche Informationen</h4>
     <div class="row">
         <div class="col-2 input-field">
-            <label for="postalCode">PLZ:</label>
+            <label for="firstname">Vorname:</label>
             <input type="text"
-                   id="postalCode" name="postalCode"
-                   placeholder="PLZ" required>
+                   id="firstname" name="firstname"
+                   placeholder="Vorname" required autofocus>
         </div>
-
         <div class="col-2 input-field">
-            <label for="city">Stadt:</label>
+            <label for="lastname">Nachname:</label>
             <input type="text"
-                   id="city" name="city"
-                   placeholder="Stadt" required>
+                   id="lastname" name="lastname"
+                   placeholder="Nachname" required>
+        </div>
+    </div>
+
+    <div class="address">
+        <label for="street">Straße:</label><br>
+        <input type="text"
+               id="street" name="street"
+               placeholder="Straße und Hausnummer" required><br>
+
+        <div class="row">
+            <div class="col-2 input-field">
+                <label for="postalCode">PLZ:</label>
+                <input type="text"
+                       id="postalCode" name="postalCode"
+                       placeholder="PLZ" required>
+            </div>
+
+            <div class="col-2 input-field">
+                <label for="city">Stadt:</label>
+                <input type="text"
+                       id="city" name="city"
+                       placeholder="Stadt" required>
+            </div>
         </div>
     </div>
 </div>
