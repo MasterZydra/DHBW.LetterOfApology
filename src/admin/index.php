1e47d499becd730957c4910fff3f4c8d18cbe6e7
trigeha <51359838+trigeha@users.noreply.github.com>
2019-11-15 16:00:24 +0100

    Changed sorting

--- src/admin/index.php
+++ src/admin/index.php
@@ -20,31 +20,6 @@
                 echo "<a href='./'><button class='button round'>Zurück</button></a>";
             }
         
-            echo "<form method='get'>";
-            
-            if(isset($_GET["folder"])) {
-                echo "<input type ='hidden' name='folder' value='".$_GET["folder"]."'>";
-            }
-        
-            echo "<input type='hidden'
-                name='sort' value=";
-            if(isset($_GET["sort"]) && $_GET["sort"] == "ASC") {
-                echo "'DESC'";
-            }
-            else {
-                echo "'ASC'";
-            }
-            echo ">
-                <button class='button round'>";
-            if(isset($_GET["sort"]) && $_GET["sort"] == "ASC") {
-                echo "Absteigend sortieren";
-            }
-            else {
-                echo "Aufsteigend sortieren";
-            }
-            echo "</button>
-                </form>";
-        
             echo "</div>";
 
             $directoryHandle = dir($directory);
@@ -58,11 +33,41 @@
                         $folders[] = $file;
                     }
                     if(isset($_GET["folder"]) && !is_dir($directory.DIRECTORY_SEPARATOR.$file)) {
-                        $files[] = $file;
+                        $files[filemtime($directory.DIRECTORY_SEPARATOR.$file)] = $file;
                     }
                 }
             }
         
+        
+        echo "<div class='list'><table><tr><th>Name";
+        echo "<form method='get'>";
+        if(isset($_GET["folder"])) {
+                echo "<input type ='hidden' name='folder' value='".$_GET["folder"]."'>";
+            }
+        echo "<input type='hidden' name='sort' value='";
+        if(isset($_GET["sort"]) && $_GET["sort"] == "ASC") {
+            echo "DESC";
+        }
+        else {
+            echo "ASC";
+        }
+        echo "'><button class='button round'>	
+&#9207;</button></form>";
+        echo "</th><th>Letzte Änderung";
+        echo "<form method='get'>";
+        if(isset($_GET["folder"])) {
+                echo "<input type ='hidden' name='folder' value='".$_GET["folder"]."'>";
+            }
+        echo "<input type='hidden' name='sort' value='";
+        if(isset($_GET["sort"]) && $_GET["sort"] == "ASCDate") {
+            echo "DESCDate";
+        }
+        else {
+            echo "ASCDate";
+        }
+        echo "'><button class='button round'>	
+&#9207;</button></form>";
+        echo "</th></tr>";
             if (!isset($_GET["folder"])) {
                 if (isset($_GET["sort"]) && $_GET["sort"] == "DESC") {
                     arsort($folders);
@@ -71,21 +76,32 @@
                     asort($folders);
                 }
                 foreach($folders as $folder) {
-                    echo "<a class='list-item' href='?folder=$folder'>$folder</a><br>";
+                    echo "<tr><td><a class='list-item' href='?folder=$folder'>$folder</a></td></tr>";
                 }
             }
             else {
                 if (isset($_GET["sort"]) && $_GET["sort"] == "DESC") {
                     arsort($files);
                 }
+                elseif (isset($_GET["sort"]) && $_GET["sort"] == "ASCDate") {
+                    ksort($files);
+                }
+                else if (isset($_GET["sort"]) && $_GET["sort"] == "DESCDate") {
+                    krsort($files);
+                }
                 else {
                     asort($files);
+                }    
+                $keys = array_keys($files);
+                foreach($keys as $key) {
+                    echo "<tr><td><a class='list-item' href='$directory/$files[$key]' target='_blank'>$files[$key]</a></td><td>".date("d.m.Y", $key)."</td></tr>";
                 }
-                foreach($files as $file) {
-                    echo "<a class='list-item' href='$directory/$file' target='_blank'>$file</a><br>";
-                }
+                
+
             }
 
+        echo "</table></div>";
+            
             $directoryHandle->close();
             ?>
 
