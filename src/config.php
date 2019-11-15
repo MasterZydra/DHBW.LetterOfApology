6d8685fbc5bdd80060c4d83b28ae073809995d0f
David Hein <david-hein196@web.de>
2019-11-15 15:43:37 +0100

    Add comments

--- src/config.php
+++ src/config.php
@@ -1,9 +1,17 @@
 <?php
+    // Created by 6439456
 
-$config['receiver_name'] = "Akademie für Muster";
-$config['receiver_street'] = "Musterstraße 1";
-$config['receiver_postalCode'] = "12345";
-$config['receiver_city'] = "Musterstadt";
-$config['salutation'] = "Sehr geehrter Herr Mustermann";
-
+    // --- Configuration for PDF document ---
+    // --- Receiver data ---
+    // - Name of receiver
+    $config['receiver_name'] = "Akademie für Muster";
+    // - Street and house number of receiver
+    $config['receiver_street'] = "Musterstraße 1";
+    // - Postal code of receiver
+    $config['receiver_postalCode'] = "12345";
+    // - City of receiver
+    $config['receiver_city'] = "Musterstadt";
+    
+    // --- Salutation ---
+    $config['salutation'] = "Sehr geehrter Herr Mustermann";
 ?>
