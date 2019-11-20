f7138e55ffd37dfd13286996f33f52ef303abd05
David Hein <david-hein196@web.de>
2019-11-20 18:01:11 +0100

    Switch order so dark mode is used correctly in Safari

--- src/admin/index.php
+++ src/admin/index.php
@@ -3,8 +3,8 @@
     <head>
         <meta charset="UTF-8">
         <title>Admin</title>
-        <link href="admin.css" rel="stylesheet">
         <link href="../css/base.css" rel="stylesheet">
+        <link href="admin.css" rel="stylesheet">
     </head>
     <body>
 <?php
