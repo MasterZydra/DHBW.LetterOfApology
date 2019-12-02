b3e1331b147c8ff2b108ea747ad1db6070fc9eaa
David Hein <david-hein196@web.de>
2019-12-02 18:32:51 +0100

    Add logic to delete a file

--- src/admin/deleteMessage.php
+++ src/admin/deleteMessage.php
@@ -11,20 +11,39 @@
     // Set variable level to go one level up to index.php
     $level = 1;
     include("../navbar.php");
+    if (!isset($_GET["delete"])) {
 ?>
-        <div class="container content">
-            <form action="" method="POST">
-                Wollen Sie die Datei wirklich löschen?
-            </form>
-            <a href=''>
-                <button class='button round'>Ja</button>
-            </a>
-            <a href='index.php?folder=<?php
-    echo $_GET['folder'];
-    if (isset($_GET['sort'])) echo "&sort=$_GET[sort]";
-            ?>'>
-                <button class='button round'>Nein</button>
-            </a>
-        </div>
-    </body>
+    <div class="container content">
+        <form action="" method="POST">
+            Wollen Sie die Datei wirklich löschen?
+        </form>
+        <a href='deleteMessage.php?delete=1&folder=<?php
+            echo $_GET['folder'];
+            echo "&file=$_GET[file]";
+            if (isset($_GET['sort'])) echo "&sort=$_GET[sort]";
+        ?>'>
+            <button class='button round'>Ja</button>
+        </a>
+        <a href='index.php?folder=<?php
+            echo $_GET['folder'];
+            if (isset($_GET['sort'])) echo "&sort=$_GET[sort]";
+        ?>'>
+            <button class='button round'>Nein</button>
+        </a>
+    </div>
+<?php
+    } else {
+        if (!isset($_GET['file'])) {
+            echo "File not given!";
+        }
+        if (!unlink($_GET['file'])) {  
+            echo ("$_GET[file] cannot be deleted due to an error");  
+        }
+        
+        $dest = "index.php?folder=$_GET[folder]&sort=$_GET[sort]";
+        header("Location: " . $dest);
+        exit();
+    }     
+?>
+</body>
 </html>
