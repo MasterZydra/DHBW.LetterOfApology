4c0780ff52ac373220afa0a864ddd34d342555b4
David Hein <david-hein196@web.de>
2019-12-03 20:24:01 +0100

    HTML Validator: Fix empty div element

--- src/navbar.php
+++ src/navbar.php
@@ -22,7 +22,6 @@
         <div>
             <a class="navbar-title" href=".<?php echo isset($level) ? getBackPath($level) : ""; ?>/"><h1>Entschuldigungs-Generator</h1></a>
         </div>
-        <div>
         <?php
             // remove everything after '?' in URL if it exists
             $url = $_SERVER['REQUEST_URI'];
@@ -36,9 +35,8 @@
             $lastFolderInUrl = array_values(array_slice($urlArray, -1))[0];
             if ($lastFolderInUrl != "admin") {
                 // only show admin button if you are not in the admin space
-                echo "<a class='button round admin-button' href='admin/'>Admin</a>";
+                echo "<div><a class='button round admin-button' href='admin/'>Admin</a></div>";
             }
         ?>
-        </div>
     </div>
 </nav>
